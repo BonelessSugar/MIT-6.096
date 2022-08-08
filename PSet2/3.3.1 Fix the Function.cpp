fd785fc2b8942ffb75e1a5add6080270ad40b942
BonelessSugar <106636487+BonelessSugar@users.noreply.github.com>
2022-08-08 15:03:52 -0400

    Create 3.3.1 Fix the Function.cpp

---
+++ PSet2/3.3.1 Fix the Function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void doubleNumber(int &num) {num = num * 2;}
+
+int main() {
+    int num = 35;
+    doubleNumber(num);
+    std::cout << num; // Should print 70
+    return 0;
+}
